399cd523f073b391cd0ef2a87f2ba3333e1fd2bf
Malik Warren <Mawar9@morgan.edu>
2019-10-15 14:30:01 -0400

    update env files

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 import os, flask, flask_socketio, flask_sqlalchemy, psycopg2, app
 
-app = flask.Flask(__name__)
-socketio = flask_socketio.SocketIO(app)
+#app = flask.Flask(__name__)
+#socketio = flask_socketio.SocketIO(app)
+
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://leaky:admin@localhost/postgres' 
 app.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 db = flask_sqlalchemy.SQLAlchemy(app.app)
-# creates a database object
+# creates a database objectç
 
 
 class Message(db.Model): 
